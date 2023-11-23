b9c2ca732e299946ecb9e1dfd84d1f5eb6a7656a
moose-code <jonjonclark@gmail.com>
2023-11-23 12:48:32 +0000

    Add basic user entity and stats

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -36,6 +36,7 @@ import {
   UnpausedEntity,
   UpgradedEntity,
   EventsSummaryEntity,
+  UserEntity,
 } from "./src/Types.gen";
 
 const GLOBAL_EVENTS_SUMMARY_KEY = "GlobalEventsSummary";
@@ -53,6 +54,94 @@ const INITIAL_EVENTS_SUMMARY: EventsSummaryEntity = {
   unpausedsCount: BigInt(0),
   upgradedsCount: BigInt(0),
 };
+
+const INITIAL_USER: UserEntity = {
+  id: "0x000000",
+  totalNumberOfDeposits: BigInt(0),
+  numberOfETHDeposits: BigInt(0),
+  numberOfUSDDeposits: BigInt(0),
+};
+
+LaunchBridgeContract_ETHDeposited_loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+  context.User.load(event.params.user.toString());
+});
+
+LaunchBridgeContract_ETHDeposited_handler(({ event, context }) => {
+  let summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+  let user = context.User.get(event.params.user.toString());
+
+  let currentUserEntity: UserEntity = user ?? INITIAL_USER;
+
+  let nextUserEntity = {
+    ...currentUserEntity,
+    id: event.params.user.toString(),
+    totalNumberOfDeposits: currentUserEntity.totalNumberOfDeposits + BigInt(1),
+    numberOfETHDeposits: currentUserEntity.numberOfETHDeposits + BigInt(1),
+  };
+
+  let currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  let nextSummaryEntity = {
+    ...currentSummaryEntity,
+    eTHDepositedsCount: currentSummaryEntity.eTHDepositedsCount + BigInt(1),
+  };
+
+  let eTHDepositedEntity: ETHDepositedEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+    user: event.params.user,
+    shares: event.params.shares,
+    amount: event.params.amount,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+    timestamp: event.blockTimestamp,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.ETHDeposited.set(eTHDepositedEntity);
+  context.User.set(nextUserEntity);
+});
+
+LaunchBridgeContract_USDDeposited_loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+  context.User.load(event.params.user.toString());
+});
+
+LaunchBridgeContract_USDDeposited_handler(({ event, context }) => {
+  let summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+  let user = context.User.get(event.params.user.toString());
+
+  let currentUserEntity: UserEntity = user ?? INITIAL_USER;
+
+  let nextUserEntity = {
+    ...currentUserEntity,
+    id: event.params.user.toString(),
+    totalNumberOfDeposits: currentUserEntity.totalNumberOfDeposits + BigInt(1),
+    numberOfUSDDeposits: currentUserEntity.numberOfUSDDeposits + BigInt(1),
+  };
+
+  let currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  let nextSummaryEntity = {
+    ...currentSummaryEntity,
+    uSDDepositedsCount: currentSummaryEntity.uSDDepositedsCount + BigInt(1),
+  };
+
+  let uSDDepositedEntity: USDDepositedEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+    user: event.params.user,
+    shares: event.params.shares,
+    amount: event.params.amount,
+    daiAmount: event.params.daiAmount,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+    timestamp: event.blockTimestamp,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.USDDeposited.set(uSDDepositedEntity);
+  context.User.set(nextUserEntity);
+});
 
 LaunchBridgeContract_AdminChanged_loader(({ event, context }) => {
   context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
@@ -103,34 +192,6 @@ LaunchBridgeContract_BeaconUpgraded_handler(({ event, context }) => {
 
   context.EventsSummary.set(nextSummaryEntity);
   context.BeaconUpgraded.set(beaconUpgradedEntity);
-});
-
-LaunchBridgeContract_ETHDeposited_loader(({ event, context }) => {
-  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
-});
-
-LaunchBridgeContract_ETHDeposited_handler(({ event, context }) => {
-  let summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
-
-  let currentSummaryEntity: EventsSummaryEntity =
-    summary ?? INITIAL_EVENTS_SUMMARY;
-
-  let nextSummaryEntity = {
-    ...currentSummaryEntity,
-    eTHDepositedsCount: currentSummaryEntity.eTHDepositedsCount + BigInt(1),
-  };
-
-  let eTHDepositedEntity: ETHDepositedEntity = {
-    id: event.transactionHash + event.logIndex.toString(),
-    user: event.params.user,
-    shares: event.params.shares,
-    amount: event.params.amount,
-    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
-    timestamp: event.blockTimestamp,
-  };
-
-  context.EventsSummary.set(nextSummaryEntity);
-  context.ETHDeposited.set(eTHDepositedEntity);
 });
 
 LaunchBridgeContract_Initialized_loader(({ event, context }) => {
@@ -235,35 +296,6 @@ LaunchBridgeContract_Paused_handler(({ event, context }) => {
 
   context.EventsSummary.set(nextSummaryEntity);
   context.Paused.set(pausedEntity);
-});
-
-LaunchBridgeContract_USDDeposited_loader(({ event, context }) => {
-  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
-});
-
-LaunchBridgeContract_USDDeposited_handler(({ event, context }) => {
-  let summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
-
-  let currentSummaryEntity: EventsSummaryEntity =
-    summary ?? INITIAL_EVENTS_SUMMARY;
-
-  let nextSummaryEntity = {
-    ...currentSummaryEntity,
-    uSDDepositedsCount: currentSummaryEntity.uSDDepositedsCount + BigInt(1),
-  };
-
-  let uSDDepositedEntity: USDDepositedEntity = {
-    id: event.transactionHash + event.logIndex.toString(),
-    user: event.params.user,
-    shares: event.params.shares,
-    amount: event.params.amount,
-    daiAmount: event.params.daiAmount,
-    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
-    timestamp: event.blockTimestamp,
-  };
-
-  context.EventsSummary.set(nextSummaryEntity);
-  context.USDDeposited.set(uSDDepositedEntity);
 });
 
 LaunchBridgeContract_Unpaused_loader(({ event, context }) => {
